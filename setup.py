ae0525ec993edbe2c93216bccf578a672b53a249
Stan Hu <stanhu@aclimalabs.com>
2013-06-26 16:13:41 -0700

    Rename trunk to protobuf-json
    Add setup.py for pip installs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "protobuf-json",
+    version = "0.1",
+    url = 'http://github.com/stanhu/protobuf-json',
+    license = 'BSD',
+    description = "JSON serialization support for Google's protobuf Messages",
+    author = 'Paul Dovbush',
+    packages = ['protobuf-json']
+)
+
